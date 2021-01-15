c37d32cff5f401c6161cbf395fc15c1d06e33800
iamsanguine <74162401+iamsanguine@users.noreply.github.com>
2021-01-15 07:29:06 -0500

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -2,9 +2,9 @@
 
 if [[ $(docker ps -a -q) ]]
 then
-        docker stop $(docker ps -a -q)
-        docker rm $(docker ps -a -q)
-        docker rmi $(docker images -q)
+        docker stop $(docker ps -a -q |  grep applebite)
+        docker rm $(docker ps -a -q |  grep applebite)
+        docker rmi $(docker images -q |  grep applebite)
         echo "stopped and cleaned old container"
 else
         echo "nothing to clean"
