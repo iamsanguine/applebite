661e85126db68e0954816096699ad56548f9e626
iamsanguine <74162401+iamsanguine@users.noreply.github.com>
2021-01-15 07:18:51 -0500

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -4,6 +4,7 @@ if [[ $(docker ps -a -q) ]]
 then
         docker stop $(docker ps -a -q)
         docker rm $(docker ps -a -q)
+        docker rmi $(docker images -q)
         echo "stopped and cleaned old container"
 else
         echo "nothing to clean"
