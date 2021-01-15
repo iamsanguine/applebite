5de78c7b18999161d93c8d2ca6c7184589e41c58
iamsanguine <74162401+iamsanguine@users.noreply.github.com>
2021-01-15 07:31:38 -0500

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -4,8 +4,10 @@ if [[ $(docker ps -a -q) ]]
 then
         docker stop $(docker ps -a -q |  grep applebite)
         docker rm $(docker ps -a -q |  grep applebite)
+        echo "stopped and cleaned old applebite container"
+
         docker rmi $(docker images -q |  grep applebite)
-        echo "stopped and cleaned old container"
+        echo "deleted old applebite images"
 else
         echo "nothing to clean"
 fi
